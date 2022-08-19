12b2b7fcbe851b1d8ec9798db3916d011bf420e9
morimuraryo <108705136+morimuraryo@users.noreply.github.com>
2022-08-19 17:50:02 +0900

    Update 20220819073008_create_order_details.rb

--- db/migrate/20220819073008_create_order_details.rb
+++ db/migrate/20220819073008_create_order_details.rb
@@ -1,6 +1,7 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
+      t.integer :id
       t.integer:item_id
       t.integer:order_id
       t.integer:price
