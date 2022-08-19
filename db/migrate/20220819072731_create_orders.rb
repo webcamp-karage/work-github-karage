d294ee799da3b904b0c49f78ec208355cb3423dc
morimuraryo <108705136+morimuraryo@users.noreply.github.com>
2022-08-19 08:39:14 +0000

    [add teble order,order_detail]

--- db/migrate/20220819072731_create_orders.rb
+++ db/migrate/20220819072731_create_orders.rb
@@ -1,6 +1,17 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.integer:customer_id
+      t.string:postal_code
+      t.string:adress
+      t.string:name
+      t.integer:shipping_cost
+      t.integer:total_payment
+      t.integer:payment_method
+      t.integer:status
+      t.datetime:created_at
+      t.datetime:updated_at
+
 
       t.timestamps
     end
