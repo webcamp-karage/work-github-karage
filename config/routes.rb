5be7f33e924e480545704383827311ab0e48d55a
morimuraryo <108705136+morimuraryo@users.noreply.github.com>
2022-08-25 07:50:01 +0000

    [add routes,view]

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,13 @@ Rails.application.routes.draw do
   namespace :public do
     resources :homes
     resources:items
-    resources:customers
+    get 'customers/my_page' => 'customers#show', as: "customer"
+    get 'customers/information/edit' => 'customers#edit', as: "customers_edit"
+    patch 'customers/information' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    post 'orders/confirm', to: 'orders#confirm'
+    get "orders/complete" => "orders#complete", as: "complete"
     resources:cart_itmes
     resources:orders
     resources:addresses
