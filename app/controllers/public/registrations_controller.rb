f6993711994c2e8a68af06f8193bfc8153b12678
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-08-26 14:02:04 +0000

    delete

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -54,6 +54,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def after_sign_up_path_for(resource)
+    public_customer_path(resource)
+  end
+  
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
