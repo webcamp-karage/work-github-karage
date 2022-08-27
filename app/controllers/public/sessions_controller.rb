fbbbd264e7c273ef90d7506b6f0263321f46e514
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-08-27 08:37:37 +0000

    public

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :find_deleted_customer, only: [:create]
+  before_action :reject_inactive_customer, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -26,7 +26,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   def after_sign_in_path_for(resource)
-    public_customer__path
+    public_customer_path
   end
 
   def after_sign_out_path_for(resource)
