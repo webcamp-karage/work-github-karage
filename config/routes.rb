7ff18364e0b78af6a4a205f08a0c47417971e1fd
kazuki-05 <ec2-user@ip-172-31-9-243.ap-northeast-1.compute.internal>
2022-08-27 13:29:46 +0000

    model order

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
    resources:genres, only: [:index,:create,:edit, :update]
    resources:customers, only: [:index, :show, :edit, :update]
 
-   resources:orders
-   resources:order_details
+   resources:orders, only: [:show, :update]
+   resources:order_details, only: [:update]
  end
 
 
